d2c292b9af7ec03f700c8f5a2231c65594f72d8f
Ortiv Inga <ortiv@programmer.net>
2019-02-20 22:35:39 -0500

    add os import application run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect,jsonify, url_for, flash
-
+from os import environ
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Cuisine, Restaurant, MenuItem, User
